72958b60cfde2fe0817db26531cbbeb0c7d78c3a
Nazish Ahmad <nazish.code@gmail.com>
2026-09-13 13:05:03 +0530

    task: part1- 1.5: use the single object for data

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,5 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
 };
 
 const Part = ({ part }) => {
@@ -30,27 +30,29 @@ const Total = ({ parts }) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
