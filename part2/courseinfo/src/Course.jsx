6a3d9c64fe2339ee77aa4d1b418589222cf75bc6
Nazish Ahmad <nazish.code@gmail.com>
2026-09-14 19:46:15 +0530

    task : 2.1: Course information step 6

---
+++ part2/courseinfo/src/Course.jsx
@@ -0,0 +1,31 @@
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+function Course({ course }) {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  );
+}
+
+export default Course;
