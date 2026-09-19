a8415532f93becea01ec6ced5830cee85ecb70e8
Nazish Ahmad <nazish.code@gmail.com>
2026-09-19 20:14:55 +0530

    task : 2.8: The Phonebook Step 3 option to add phone number

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
@@ -12,9 +13,11 @@ const App = () => {
     }
     const nameObject = {
       name: newName,
+      number: newNumber,
     };
     setPersons(persons.concat(nameObject));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -26,12 +29,21 @@ const App = () => {
           <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          number:{" "}
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person, index) => (
-        <p key={person.name + index}>{person.name}</p>
+        <div key={person.name + index}>
+          <span>{person.name}</span> <span>{person.number}</span>
+        </div>
       ))}
     </div>
   );
