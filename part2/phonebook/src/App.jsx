2de7127edd6ca6a883258378f8ed66119562542a
Nazish Ahmad <nazish.code@gmail.com>
2026-09-19 20:10:28 +0530

    task : 2.7: The Phonebook Step 2 prevent duplicate names

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,6 +6,10 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault();
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
     const nameObject = {
       name: newName,
     };
